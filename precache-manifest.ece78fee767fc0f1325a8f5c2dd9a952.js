2e0feab0c693f3483ab6918490962954e6a85068
unknown <vandyliu1999@gmail.com>
2019-09-01 21:48:35 -0700

    Updates

---
+++ precache-manifest.ece78fee767fc0f1325a8f5c2dd9a952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe0a80e318f21da2b4219415468cd73",
+    "url": "/boxscorequick/index.html"
+  },
+  {
+    "revision": "aaefd50dcabe0cabc2e5",
+    "url": "/boxscorequick/static/css/2.034e63f8.chunk.css"
+  },
+  {
+    "revision": "63617be94adf0b8062b7",
+    "url": "/boxscorequick/static/css/main.c2b47e8d.chunk.css"
+  },
+  {
+    "revision": "aaefd50dcabe0cabc2e5",
+    "url": "/boxscorequick/static/js/2.5017cc12.chunk.js"
+  },
+  {
+    "revision": "63617be94adf0b8062b7",
+    "url": "/boxscorequick/static/js/main.b5dc7008.chunk.js"
+  },
+  {
+    "revision": "6fc7fc928ef862791340",
+    "url": "/boxscorequick/static/js/runtime~main.0f967ef8.js"
+  }
+]);
